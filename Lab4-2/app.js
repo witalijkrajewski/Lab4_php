6b58360a43b212656f1bc1f0047e8e6939c0a779
witalij.krajewski <witalijkrajewski@gmail.com>
2023-03-11 11:28:35 +0300

    fix commit

--- Lab4-2/app.js
+++ Lab4-2/app.js
@@ -1,18 +1,19 @@
 function validateForm() {
     const checkboxes = document.querySelectorAll('input[type=checkbox]');
+    let checked_checkboxes_counter = 0;
 
 
-    let checked = false;
     for (let i = 0; i < checkboxes.length; i++) {
         if (checkboxes[i].checked) {
-            checked = true;
-            break;
+            checked_checkboxes_counter += 1;
+            if (checked_checkboxes_counter === 2) {
+                break;
+            }
         }
     }
 
-
-    if (!checked) {
-        alert('You must select at least one course!');
+    if (checked_checkboxes_counter < 2) {
+        alert('You must select at least two courses')
         return false;
     }
 }
